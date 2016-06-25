f7492726de8d33bef882bf42d1fb72ca7fd47989
iOSYeGuang <2908534549@qq.com>
2016-06-24 20:08:23 -0700

    sqs222
    fnsfjopjf jojdp[fskmfo;jp[gfvswml;job

---
+++ TestDemo006/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo006
+//
+//  Created by Mac on 16/6/24.
+//  Copyright © 2016年 surker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
